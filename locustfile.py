18e97c5ef537468bba47f58a9cd4aaa947d4b4cf
Faraz <faraz.fatahnaie@gmail.com>
2024-05-10 14:37:43 +0330

    locust test plan refactor

--- locustfile.py
+++ locustfile.py
@@ -1,96 +1,133 @@
-from locust import HttpUser, task, between, SequentialTaskSet
+from locust import HttpUser, task, SequentialTaskSet
+from dotenv import load_dotenv
 import json
 import time
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 
-class MyUser(SequentialTaskSet):
-    # wait_time = between(3, 4)  # wait time between tasks
+class KnowMeSequenceOfTasks(SequentialTaskSet):
 
     def on_start(self):
         self.user_data = {}
-        self.private_key = "LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlCT3dJQkFBSkJBSXZrdklTWUtZMS9FMHcyL2V0Q09GV3FaNzVMblIrUEV6K0Q0bDkvcTlBL0JvSmJ4bmVlCmg0N3JoSkdMaVZFWEs4cGM5dTBaMmZlMzU4R0YrVUJFTUhFQ0F3RUFBUUpBT2VpbzVSWjc5UGZLakQwalpWU1gKTDFjSDlPMG1FSjlRYjNWdnF1cVZaS2h6cjB6YlVFMnBYcSswb29TLzJ2UWQvNE9hQnNRM084Wm5kTi9wVWtNVQoxUUlqQU5MY2phcWRtQTZteTJQdllJRjA3MHphbHRRSmJyTnpaOEtrTnRKRFl5SnJIeXNDSHdDcDF3NUl2K1JhClVDWEhhR21HOWNPbHJpZngrWnc2eGtid2kyVXNnTk1DSW5abFBHNDJPckRQV3Boc1NpV21RTTlJVlRRTmI4ajIKM2FYZWlxR1pFTXE4bHkwQ0hua2o3SXRWVzdKVFFtOFYrVmNMQ3U2czVzOEFSRC9qMXd1UjBhdEpnd0lpTjdJQwpaR1duM3B3T3lzd0VtTFNHODUwRlpBbHBzbUk2WkwycVlRcThOQ0hQWmc9PQotLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQo="
-        self.public_key = "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1FZ0NRUUNMNUx5RW1DbU5meE5NTnYzclFqaFZxbWUrUzUwZmp4TS9nK0pmZjZ2UVB3YUNXOFozbm9lTzY0U1IKaTRsUkZ5dktYUGJ0R2RuM3QrZkJoZmxBUkRCeEFnTUJBQUU9Ci0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K"
-        self.customer_id = "2d2b50ce-0889-4165-9714-0b835fd7cf2f"
+        self.private_key = os.getenv("PRIVATE_KEY")
+        self.public_key = os.getenv("PUBLIC_KEY")
+        self.customer_id = os.getenv("CUSTOMER_ID")
         self.request_count = 0
 
     @task
     def create_and_stop_session(self):
         try:
-            print('==============================================')
+            print('================= NEW SESSION ===================')
+            create_session_res = self.create_session()
 
-            # POST request to create session
-            url_post = '/sessions/'
-            headers_post = {'Content-Type': 'application/json'}
-            data_post = {
-                "customer_id": self.customer_id,
-                "service_codename": "liveness-verification-001",
-                "national_code": "0021219958",
-                "birth_date": "1377-04-24"
-            }
+            if create_session_res.status_code == 201:
+                self.register_variable(create_session_res)
 
-            response = self.client.post(url_post, headers=headers_post, data=json.dumps(data_post))
+                session_verification_res = self.session_verification()
+                if session_verification_res.status_code == 200:
+                    self.register_variable(session_verification_res)
 
-            if response.status_code == 201:
-                self.register_variable(response)
-                session_token = self.user_data.get('token')
+                    upload_asset_res = self.upload_asset()
 
-                # GET request to verify session creation
-                url_get = f'/sessions/verify?token={session_token}'
-                response = self.client.get(url_get)
+                    if upload_asset_res.status_code == 200:
 
-                if response.status_code == 200:
-                    self.register_variable(response)
-                    session_id = self.user_data.get('session_id')
-
-                    # PATCH request to send the image/video content
-                    url_patch = f'/sessions/session/{session_id}/'
-                    files = {
-                        'query_face': open(str('/home/faraz/Downloads/faraz.jpg'), 'rb'),
-                        'video_file': open(str('/home/faraz/Downloads/faraz.webm'), 'rb')
-                    }
-
-
-                    data_patch = {
-                        'public_key': str(self.public_key)
-                    }
-
-                    response = self.client.patch(url_patch, files=files, data=data_patch, stream=True)
-
-                    if response.status_code == 200:
-                        self.register_variable(response)
+                        self.register_variable(upload_asset_res)
 
                         # Wait for 5 seconds before stopping the session
                         time.sleep(5)
-                        self.stop_session()
+                        validate_make_result_res = self.validate_make_result()
+
+                        if validate_make_result_res.status_code == 200:
+                            # TODO: check the format of the respone (in queue or finished)
+                            self.register_variable(validate_make_result_res)
+                            print('+++++++++++++ END SESSION +++++++++++++++')
+                        else:
+                            print(
+                                f"Error in POST request - validate_make_result: {validate_make_result_res.status_code}")
 
                     else:
-                        print(f"Error in PATCH request: {response.status_code}")
+                        print(f"Error in PATCH request - upload_asset: {upload_asset_res.status_code}")
                 else:
-                    print(f"Error in GET request for session verification: {response.status_code}")
+                    print(f"Error in GET request - session_verification: {session_verification_res.status_code}")
+            else:
+                print(f"Error in POST request - create_session: {create_session_res.status_code}")
 
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    def stop_session(self):
-        print('++++++++++++++++++++++++++++++++++++++=')
+    def create_session(self):
+        try:
+            # POST request to create session
+            url_post = '/sessions/'
+            headers_post = {'Content-Type': 'application/json', 'CUSTOMER-ID': f'{self.customer_id}'}
+            data_post = {
+                "service_codename": "liveness-verification-001",
+                "national_code": "0021219958",
+                "birthdate": 900460800
+            }
+            response = self.client.post(url_post, headers=headers_post, data=json.dumps(data_post))
+            return response
+
+        except Exception as e:
+            print(f"An error occurred in create_session: {e}")
+
+    def session_verification(self):
+        try:
+            session_token = self.user_data.get('token')
+
+            # GET request to verify session creation
+            url_get = f'/sessions/verify?token={session_token}'
+            headers_get = {'PRIVATE-KEY': str(self.private_key), 'CUSTOMER-ID': str(self.customer_id)}
+            response = self.client.get(url_get, headers=headers_get)
+            return response
+
+        except Exception as e:
+            print(f"An error occurred in session_verification: {e}")
+
+    def upload_asset(self):
+        try:
+            session_id = self.user_data.get('session_id')
+
+            # PATCH request to send the image/video content
+            url_patch = f'/sessions/session/{session_id}/'
+            files = {
+                'video_file': open(str('file/0021219958.webm'), 'rb')
+            }
+
+            data_patch = {
+                'public_key': str(self.public_key)
+            }
+            headers_patch = {'CUSTOMER-ID': f'{self.customer_id}'}
+
+            response = self.client.patch(url_patch,
+                                         headers=headers_patch,
+                                         files=files,
+                                         data=data_patch,
+                                         stream=True)
+            return response
+
+        except Exception as e:
+            print(f"An error occurred in upload_asset: {e}")
+
+    def validate_make_result(self):
         try:
             validation_token = self.user_data.get('validation_token')
-            if validation_token:
-                url_post = '/sessions/vamr/?timeout=30'
-                headers_post = {'Content-Type': 'application/json'}
-                data_post = {
-                    "validation_token": validation_token,
-                    "private_key": self.private_key
-                }
 
-                result_response = self.client.post(url_post, headers=headers_post, data=json.dumps(data_post))
-                if result_response.status_code == 200:
-                    self.register_variable(result_response)
-                    print("Process completed successfully.")
-                else:
-                    print(f"Error in POST request for result: {result_response.status_code}")
+            url_post = '/sessions/vamr/?timeout=30'
+            headers_post = {'Content-Type': 'application/json'}
+            data_post = {
+                "validation_token": validation_token,
+                "private_key": self.private_key
+            }
+
+            result_response = self.client.post(url_post, headers=headers_post, data=json.dumps(data_post))
+            return result_response
+
         except Exception as e:
-            print(f"An error occurred in on_stop: {e}")
+            print(f"An error occurred in validate_make_result: {e}")
 
     def register_variable(self, response):
         try:
@@ -120,12 +157,8 @@ class MyUser(SequentialTaskSet):
         return "random_company"
 
 
-class UserBehaviour(SequentialTaskSet):
-    tasks = [MyUser]
-
-
 class LoggedInUser(HttpUser):
-    host = "http://localhost"
+    host = "https://api.face-kyc.ir"
     # wait_time = between(4, 5)
-    tasks = [UserBehaviour]
+    tasks = [KnowMeSequenceOfTasks]
     # max_requests = 5
